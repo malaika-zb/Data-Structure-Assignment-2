02e2f556c7787ef4c3d2f645b70899fe231c151b
Malaika Zainab <malaikazainab@gmail.com>
2024-10-19 00:02:00 +0500

    make node class for multi dim

--- game.cpp
+++ game.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+#include<ncurses.h>
 using namespace std;
+
+class Node{
+public:
+//all the nodes for the multidim grid
+char value;
+Node* up;
+Node* down;
+Node* left;
+Node* right;
+
+//constructor
+Node(char val): value(val), up(nullptr), down(nullptr), left(nullptr), right(nullptr)
+{}
+};  
+
+
 
 int main()
 {
